e0b8f1710ab2886115026a70ffc711ac124b1344
anri103 <ianrianri@gmail.com>
2024-02-07 18:25:37 +0300

    -

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////
+// [ Swiper Sliders ]
+
+var swiperHeroSlider = new Swiper(".swiperHeroSlider", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 16,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiperHeroSlider-next",
+        prevEl: ".swiperHeroSlider-prev",
+    },
+});
+
+var swiperCategoriesCircles = new Swiper(".swiperCategoriesCircles", {
+    slidesPerView: 5,
+    loop: false,
+    spaceBetween: 32,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiperCategoriesCircles-next",
+        prevEl: ".swiperCategoriesCircles-prev",
+    },
+});
+
+
+//////////////////////////////////////////////////////////////////
+// [ Enable Fancybox ]
+
+Fancybox.bind("[data-fancybox]", {
+    Thumbs: {
+        type: "classic",
+    },
+    Toolbar: {
+        display: {
+            left: [],
+            middle: [],
+            right: ["close"],
+        },
+    },
+});
