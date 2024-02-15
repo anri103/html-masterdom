2abb7caf13f7bf6025246cecee52c35377a48dcf
anri103 <ianrianri@gmail.com>
2024-02-15 18:39:49 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,118 +1,121 @@
-//////////////////////////////////////////////////////////////////
-// [ Swiper Sliders ]
-
-var swiperHeroSlider = new Swiper(".swiperHeroSlider", {
-    slidesPerView: 1,
-    loop: false,
-    spaceBetween: 16,
-    grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    navigation: {
-        nextEl: ".swiperHeroSlider-next",
-        prevEl: ".swiperHeroSlider-prev",
-    },
-});
-
-var swiperCategoriesCircles = new Swiper(".swiperCategoriesCircles", {
-    slidesPerView: "auto",
-    loop: false,
-    spaceBetween: 32,
-    grabCursor: true,
-    freeMode: true,
-    slidesOffsetBefore: 12,
-    slidesOffsetAfter: 12,
-    navigation: {
-        nextEl: ".swiperCategoriesCircles-next",
-        prevEl: ".swiperCategoriesCircles-prev",
-    },
-    breakpoints: {
-        992: {
-            slidesOffsetBefore: 0,
-            slidesOffsetAfter: 0,
+document.addEventListener('DOMContentLoaded', function () {
+    //////////////////////////////////////////////////////////////////
+    // [ Swiper Sliders ]
+    
+    var swiperHeroSlider = new Swiper(".swiperHeroSlider", {
+        slidesPerView: 1,
+        loop: false,
+        spaceBetween: 16,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
         },
-    },
-});
-
-var swiperPopularProductsNav = new Swiper(".swiperPopularProductsNav", {
-    slidesPerView: "auto",
-    loop: false,
-    spaceBetween: 12,
-    grabCursor: true,
-    freeMode: true,
-    centerInsufficientSlides: false,
-    slidesOffsetBefore: 12,
-    slidesOffsetAfter: 12,
-    breakpoints: {
-        992: {
-            centerInsufficientSlides: true,
-            spaceBetween: 32,
-            slidesOffsetBefore: 12,
-            slidesOffsetAfter: 12,
+        navigation: {
+            nextEl: ".swiperHeroSlider-next",
+            prevEl: ".swiperHeroSlider-prev",
         },
-    },
-});
-
-var swiperShopsLocation = new Swiper(".swiperShopsLocation", {
-    slidesPerView: 1,
-    loop: false,
-    spaceBetween: 16,
-    grabCursor: true,
-});
-
-var swiperBlogSliderNav = new Swiper(".swiperBlogSliderNav", {
-    slidesPerView: "auto",
-    loop: false,
-    spaceBetween: 12,
-    grabCursor: true,
-    freeMode: true,
-    centerInsufficientSlides: false,
-    slidesOffsetBefore: 12,
-    slidesOffsetAfter: 12,
-    breakpoints: {
-        992: {
-            centerInsufficientSlides: true,
-            spaceBetween: 32,
-            slidesOffsetBefore: 12,
-            slidesOffsetAfter: 12,
+    });
+    
+    var swiperCategoriesCircles = new Swiper(".swiperCategoriesCircles", {
+        slidesPerView: "auto",
+        loop: false,
+        spaceBetween: 32,
+        grabCursor: true,
+        freeMode: true,
+        slidesOffsetBefore: 12,
+        slidesOffsetAfter: 12,
+        navigation: {
+            nextEl: ".swiperCategoriesCircles-next",
+            prevEl: ".swiperCategoriesCircles-prev",
         },
-    },
-});
-
-var swiperBlogSlider = new Swiper(".swiperBlogSlider", {
-    slidesPerView: "auto",
-    loop: false,
-    spaceBetween: 22,
-    grabCursor: true,
-    freeMode: true,
-    slidesOffsetBefore: 12,
-    slidesOffsetAfter: 12,
-    navigation: {
-        nextEl: ".swiperBlogSlider-next",
-        prevEl: ".swiperBlogSlider-prev",
-    },
-    breakpoints: {
-        992: {
-            spaceBetween: 34,
+        breakpoints: {
+            992: {
+                slidesOffsetBefore: 0,
+                slidesOffsetAfter: 0,
+            },
         },
-    },
-});
-
-
-
-//////////////////////////////////////////////////////////////////
-// [ Enable Fancybox ]
-
-Fancybox.bind("[data-fancybox]", {
-    Thumbs: {
-        type: "classic",
-    },
-    Toolbar: {
-        display: {
-            left: [],
-            middle: [],
-            right: ["close"],
+    });
+    
+    var swiperPopularProductsNav = new Swiper(".swiperPopularProductsNav", {
+        slidesPerView: "auto",
+        loop: false,
+        spaceBetween: 12,
+        grabCursor: true,
+        freeMode: true,
+        centerInsufficientSlides: false,
+        slidesOffsetBefore: 12,
+        slidesOffsetAfter: 12,
+        breakpoints: {
+            992: {
+                centerInsufficientSlides: true,
+                spaceBetween: 32,
+                slidesOffsetBefore: 12,
+                slidesOffsetAfter: 12,
+            },
         },
-    },
-});
+    });
+    
+    var swiperShopsLocation = new Swiper(".swiperShopsLocation", {
+        slidesPerView: 1,
+        loop: false,
+        spaceBetween: 16,
+        grabCursor: true,
+    });
+    
+    var swiperBlogSliderNav = new Swiper(".swiperBlogSliderNav", {
+        slidesPerView: "auto",
+        loop: false,
+        spaceBetween: 12,
+        grabCursor: true,
+        freeMode: true,
+        centerInsufficientSlides: false,
+        slidesOffsetBefore: 12,
+        slidesOffsetAfter: 12,
+        breakpoints: {
+            992: {
+                centerInsufficientSlides: true,
+                spaceBetween: 32,
+                slidesOffsetBefore: 12,
+                slidesOffsetAfter: 12,
+            },
+        },
+    });
+    
+    var swiperBlogSlider = new Swiper(".swiperBlogSlider", {
+        slidesPerView: "auto",
+        loop: false,
+        spaceBetween: 22,
+        grabCursor: true,
+        freeMode: true,
+        slidesOffsetBefore: 12,
+        slidesOffsetAfter: 12,
+        navigation: {
+            nextEl: ".swiperBlogSlider-next",
+            prevEl: ".swiperBlogSlider-prev",
+        },
+        breakpoints: {
+            992: {
+                spaceBetween: 34,
+            },
+        },
+    });
+    
+    
+    
+    //////////////////////////////////////////////////////////////////
+    // [ Enable Fancybox ]
+    
+    Fancybox.bind("[data-fancybox]", {
+        Thumbs: {
+            type: "classic",
+        },
+        Toolbar: {
+            display: {
+                left: [],
+                middle: [],
+                right: ["close"],
+            },
+        },
+    });
+    
+})
